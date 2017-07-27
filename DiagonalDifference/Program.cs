3fc82a139493d451b15a95dd48ddc7a34cdcf023
Anton <anton.abyzov@gmail.com>
2017-07-27 12:25:25 +0300

    diagonal diff

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[][] a = new int[n][];
+        for (int a_i = 0; a_i < n; a_i++)
+        {
+            string[] a_temp = Console.ReadLine().Split(' ');
+            a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
+        }
+        var diag1 = 0;
+        var diag2 = 0;
+        for (var i = 0; i < n; i++)
+        {
+            diag1 += a[i][i];
+            diag2 += a[i][n - 1 - i];
+        }
+        Console.WriteLine(Math.Abs(diag1 - diag2));
+    }
+}
