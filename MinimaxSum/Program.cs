2c9b740bb6e750247375aadb75272f19e7babd0d
Anton <anton.abyzov@gmail.com>
2017-07-29 01:13:13 +0300

    minimax sum resolved

---
+++ MinimaxSum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        long[] arr = Array.ConvertAll(arr_temp, Int64.Parse);
+        var min = arr.Sum() - arr.Max();
+        var max = arr.Sum() - arr.Min();
+        Console.WriteLine("{0} {1}", min, max);
+    }
+}
