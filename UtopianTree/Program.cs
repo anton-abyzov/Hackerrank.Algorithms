c7d8badc9fbe9f222f26a26d0facc6528b6cb9fe
Anton <anton.abyzov@gmail.com>
2017-07-29 00:10:43 +0300

    utipian tree solved

---
+++ UtopianTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                    result *= 2;
+                else
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
