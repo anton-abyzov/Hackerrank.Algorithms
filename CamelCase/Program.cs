63a0747efc8e4736acd009d5c46187d25cc9a63e
Anton <anton.abyzov@gmail.com>
2017-07-29 01:18:41 +0300

    camelcase solved

---
+++ CamelCase/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        var counter = 1;
+        foreach (var character in s)
+        {
+            if (character >= 65 && character <= 90)
+                counter++;
+        }
+        Console.WriteLine(counter);
+    }
+}
