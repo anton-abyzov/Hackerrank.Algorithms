99df51728ed215cf9f063a58920a38e61a71bf66
Anton <anton.abyzov@gmail.com>
2017-07-29 02:39:08 +0300

    equalize array

---
+++ EqualizeArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine());
+        for (var i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine().Split(' ');
+            var array = Array.ConvertAll(line, int.Parse);
+            var maxElementCount = array.GroupBy(x => x).Max(x => x.Count());
+            Console.WriteLine(n - maxElementCount);
+        }
+    }
+} 
