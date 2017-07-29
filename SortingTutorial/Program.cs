f909562c7cb2b347d509d51e1697fd738586ab3b
Anton Abyzov <anton.abyzov@gmail.com>
2017-07-30 01:05:59 +0300

    sorting tutorial done

---
+++ SortingTutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var v = Convert.ToInt32(Console.ReadLine());
+        var n = Convert.ToInt32(Console.ReadLine());
+        var line = Console.ReadLine().Split(' ');
+        var array = Array.ConvertAll(line, int.Parse);
+        var index = Array.BinarySearch(array, v);
+        Console.WriteLine(index);
+    }
+}
