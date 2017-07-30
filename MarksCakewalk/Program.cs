93ab77b4b6c3929339644cf1f814e7e5dc7b7f15
Anton <anton.abyzov@gmail.com>
2017-07-31 02:30:13 +0300

    mark's cakewalk

---
+++ MarksCakewalk/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] calories_temp = Console.ReadLine().Split(' ');
+        int[] calories = Array.ConvertAll(calories_temp, Int32.Parse);
+        calories = calories.OrderByDescending(x => x).ToArray();
+        var miles = Enumerable.Range(0, n).Sum(x => Math.Pow(2, x) * calories[x]);
+        Console.WriteLine(miles);
+    }
+}
