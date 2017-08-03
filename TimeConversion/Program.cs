3d378095d38a27bbd09d3115936250809d274551
Anton <anton.abyzov@gmail.com>
2017-08-04 02:28:55 +0300

    time conversion

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static string timeConversion(string s)
+    {
+        var dateTime = DateTime.ParseExact(s, "hh:mm:sstt",CultureInfo.InvariantCulture);
+        return dateTime.ToString("HH:mm:ss");
+    }
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        string result = timeConversion(s);
+        Console.WriteLine(result);
+    }
+}
