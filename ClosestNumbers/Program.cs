b15542a95bd5601619bd6116b56eb1a9bf7bb87e
Anton <anton.abyzov@gmail.com>
2017-08-04 21:40:39 +0300

    closest numbers

---
+++ ClosestNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClosestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var line = Console.ReadLine().Split(' ');
+            var arr = Array.ConvertAll(line, int.Parse);
+            Array.Sort(arr);
+            var minDiff = arr[1] - arr[0];
+            for (int i = 2; i < arr.Length; i++)
+            {
+                minDiff = Math.Min(minDiff, Math.Abs(arr[i] - arr[i - 1]));
+            }
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (Math.Abs(arr[i] - arr[i - 1]) == minDiff)
+                {
+                    Console.Write(arr[i - 1] + " " + arr[i] + " ");
+                }
+            }
+        }
+    }
+}
