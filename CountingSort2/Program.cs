f7f63dddbeb0e165281e06034d14367d1b4a24d8
Anton <anton.abyzov@gmail.com>
2017-08-04 20:16:37 +0300

    counting sort 2

---
+++ CountingSort2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CountingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var lines = Console.ReadLine().Split(' ');
+            var arr = Array.ConvertAll(lines, int.Parse);
+            var countingArr = new int[100];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                countingArr[arr[i]]++;
+            }
+            //for (int i = 1; i < countingArr.Length; i++)
+            //{
+            //    countingArr[i] += countingArr[i - 1];
+            //}
+            for (int i = 0; i < countingArr.Length; i++)
+            {
+                for (int j = 0; j < countingArr[i]; j++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            var result = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    result[countingArr[arr[i]]-1] = arr[i];
+            //    result[countingArr[arr[i]]-1]--;
+            //}
+
+        }
+    }
+}
