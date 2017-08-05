96fdc10544eaaf72433a7a19f170a9f70730ef9b
Anton <anton.abyzov@gmail.com>
2017-08-06 02:51:27 +0300

    mars exploration

---
+++ MarsExploration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string S = Console.ReadLine();
+        var sosCount = S.Length / 3;
+        var sosstring = "";
+        for (int i = 0; i < sosCount; i++)
+        {
+            sosstring += "SOS";
+        }
+        var counter = 0;
+        for (int i = 0; i < S.Length; i++)
+        {
+            if (S[i] != sosstring[i])
+                counter++;
+        }
+        Console.WriteLine(counter);
+    }
+}
