ae0f9614acecc65a757d86221217ac4beb38986b
Anton <anton.abyzov@gmail.com>
2017-08-07 08:08:22 +0300

    anagram simplified

--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -13,21 +13,11 @@ class Solution
             return -1;
         var s1 = s.Substring(0, s.Length / 2);
         var s2 = s.Substring(s.Length / 2, s.Length / 2);
-        var s1Set = new HashSet<char>();
-        var s2Set = new HashSet<char>();
-        foreach (var ch1 in s1)
-        {
-            s1Set.Add(ch1);
-        }
-        foreach (var ch2 in s2)
-        {
-            s2Set.Add(ch2);
-        }
-        s1Set.IntersectWith(s2Set);
+        
         var counter = 0;
         foreach (var ch in s1)
         {
-            if (!s1Set.Contains(ch) || (s1Set.Contains(ch) && !s2.Contains(ch)))
+            if (!s2.Contains(ch))
                 counter++;
             if (s2.Contains(ch))
             {
