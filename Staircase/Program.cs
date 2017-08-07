15042d748156d9fd82a5325d9b9411e1a84d6de6
Anton <anton.abyzov@gmail.com>
2017-08-07 08:16:05 +0300

    staircase

---
+++ Staircase/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (var i = n - 1; i >= 0; i--)
+        {
+            var ident = string.Join("", Enumerable.Repeat(" ", i));
+            var body = string.Join("", Enumerable.Repeat("#", n - i));
+            Console.WriteLine(ident + body);
+        }
+    }
+}
