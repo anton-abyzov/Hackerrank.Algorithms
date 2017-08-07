6068f2241bd2a734c5dc8ff19a3f81796f8433dd
Anton <anton.abyzov@gmail.com>
2017-08-07 06:26:05 +0300

    weighted uniform string unoptimized

---
+++ HackerrankInString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < q; a0++)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(Regex.IsMatch(s, "h.*a.*c.*k.*e.*r.*r.*a.*n.*k.*") ? "YES" : "NO");
+            // your code goes here
+        }
+    }
+}
---
+++ WeightedUniformString/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        int n = Convert.ToInt32(Console.ReadLine());
+        HashSet<int> weightedArray = ParseStr(s);
+        for (int a0 = 0; a0 < n; a0++)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(weightedArray.Contains(x) ? "Yes" : "No");
+        }
+    }
+
+    private static HashSet<int> ParseStr(string input)
+    {
+        var weightedArr = new HashSet<int>();
+        
+        var i = 0;
+        string seq = "";
+        do
+        {
+            var weight = 0;
+
+            if (input[i] != seq.Distinct().FirstOrDefault())
+            {
+                seq = input[i].ToString();
+            }
+            else
+            {
+                seq += input[i];
+            }
+            foreach (var ch in seq)
+            {
+                weight += ch - 'a' + 1;
+            }
+            weightedArr.Add(weight);
+        } while (++i < input.Length);
+        return weightedArr;
+    }
+}
